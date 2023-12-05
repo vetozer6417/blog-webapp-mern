b1f1e1371a77d8b63affba46628a956b42709902
vetozer6417 <vetozer6417@gmail.com>
2023-12-05 18:38:01 +0700

    Installed express, dotenv, mongoose and esm

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
+const app = express()
+app.use(express.json())
+
+app.get('/', (req,res) => {
+    res.send("Server is running...")
+})
+
+const PORT = process.env.PORT || 5000 //port number is protected in .env file
+app.listen(5000, () => console.log('Server is running on port: 5000'))
